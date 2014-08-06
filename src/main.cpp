e33517ba4a767a41736cb8786fc31a6091b589d5
marprz <marta.marprz@gmail.com>
2014-08-07 01:08:07 +0200

    Modified main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,7 @@
 #include <iostream>
-#include <fstream>
-#include <list>
-#include <string>
-#include <vector>
-#include <limits>
-#include <iomanip>
 #include <unistd.h>
-#include <tuple>
-
-#include <cmath> // square root
-#include <dirent.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 
 #include "Reader.h"
-//#include "Combinations.h"
-//#include "GaussianMatrix.h"
 
 void processSignalData();
 
@@ -25,44 +10,15 @@ int main( int argc, char* argv[] )
     Stations mStations;
     Reader reader;
     bool test = false;
-    int c = getopt( argc, argv, "fd:" );
+    int c = getopt( argc, argv, "d:" );
     switch( c ){
     case 'd':
         reader.loadFromDirectory( optarg );
         processSignalData();
         break;
-	case 'f':
-//	    loadStations( optarg, mStations );
-        test = true;
-	    break;
     default:
-	    std::cout << "Missing file name! Use parameter -f filename." << std::endl;
+	    std::cout << "Missing path to the directory! Use parameter -d." << std::endl;
 	    return -1;
     }
-/*
-    if( test )
-    {
-        std::vector< std::vector< int > > stationsComb;
-        std::vector< Station > takenStations;
-
-        if( mStations.size() > 3 )
-        {
-            std::cout << "Number of ground stations: " << mStations.size() << std::endl;
-            stationsComb = getStationsCombinations( mStations.size(), 4 );
-	        std::cout << "Number of combinations: " << stationsComb.size() << std::endl;
-            PositionsList xyzr;
-        
-            std::vector< std::vector< int > >::iterator iter; 
-        	for( iter = stationsComb.begin(); iter != stationsComb.end(); ++iter )
-	        {
-                xyzr.addPositions( solveApol( 0, 0, mStations ) );
-            }
-            xyzr.printAveragePosition();
-        }
-        else
-        {
-            std::cout << "File stations.txt have to contain more ground stations!" << std::endl;
-        }
-    }*/
     return 0;
 }
